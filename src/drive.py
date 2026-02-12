2d37b067311209284f3dfaae483737d0de67176a
Akimzhan Rakhimov <163539376+AkimzhanRakhimov@users.noreply.github.com>
2026-02-12 15:44:49 +0500

    Delete src/drive.py

--- src/drive.py
+++
@@ -1,6 +0,0 @@
-drive.mount("/content/drive")
-if os.path.exists("/content/drive/MyDrive/NNP"):
-  print("Path exist already")
-else:
-  os.mkdir("/content/drive/MyDrive/NNP")
-  print("Path has been created successfully")
