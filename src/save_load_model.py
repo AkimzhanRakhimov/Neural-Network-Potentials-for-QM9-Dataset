e32fa0d2b86b4c1cde6ac92be615657c2e2b8274
Akimzhan Rakhimov <163539376+AkimzhanRakhimov@users.noreply.github.com>
2026-02-12 15:45:24 +0500

    Delete src/save_load_model.py

--- src/save_load_model.py
+++
@@ -1,6 +0,0 @@
-torch.save(obj=model.state_dict(),f="/content/drive/MyDrive/NNP/model_1.pth")
-print("Model has been saved")
-
-model.load_state_dict(torch.load("/content/drive/MyDrive/NNP/model_1.pth"))
-print("Model has been loaded")
-print(model.state_dict())
